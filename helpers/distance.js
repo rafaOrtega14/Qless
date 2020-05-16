02d79bebfa12d658948d1ab715df9b52a44fedb9
Rafael Ortega <rafa@triptobaleares.com>
2020-05-17 01:54:06 +0200

    refactor distance formula

--- helpers/distance.js
+++ helpers/distance.js
@@ -1,15 +1,14 @@
 export const calcCrow = (lat1, lon1, lat2, lon2) => {
-    var R = 6371; // km
-    var dLat = toRad(lat2 - lat1);
-    var dLon = toRad(lon2 - lon1);
-    var lat1 = toRad(lat1);
-    var lat2 = toRad(lat2);
+    const R = 6371; // km
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+    lat1 = toRad(lat1);
+    lat2 = toRad(lat2);
 
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    return d;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
 }
 const toRad = (Value) => {
     return Value * Math.PI / 180;
